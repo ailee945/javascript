2205d0365505f0739499ed566ba573e71ca1e0d3
“Ailee” <ailee945@outlook.com>
2022-04-07 23:00:19 +0800

    this-纯函数

--- 07_this其他补充/11_this面试题三.js
+++ 07_this其他补充/11_this面试题三.js
@@ -18,21 +18,21 @@ function Person (name) {
   }
 }
 
-var person1 = new Person('person1')
-var person2 = new Person('person2')
+var person1 = new Person('person1') // person1 = { name: person1, foo1...}
+var person2 = new Person('person2') // person1 = { name: person2, foo1...}
 
-person1.foo1() // person1
-person1.foo1.call(person2) // person2(显示高于隐式绑定)
+person1.foo1() // person1 隐式绑定
+person1.foo1.call(person2) // person2 显式绑定
 
-person1.foo2() // person1 (上层作用域中的this是person1)
-person1.foo2.call(person2) // person1 (上层作用域中的this是person1)
+person1.foo2() // window ✕ person1 上层作用域是person1
+person1.foo2.call(person2) // window ✕ person1 call不绑定箭头函数的this 上层作用域是person
 
-person1.foo3()() // window(独立函数调用)
-person1.foo3.call(person2)() // window
-person1.foo3().call(person2) // person2
+person1.foo3()() // window 独立函数调用
+person1.foo3.call(person2)() // person2 ✕ window 独立函数调用
+person1.foo3().call(person2) // person2 显式绑定
 
-person1.foo4()() // person1
-person1.foo4.call(person2)() // person2
+person1.foo4()() // person1 
+person1.foo4.call(person2)() // person1 ✕ person2 已经给foo4显式绑定person2
 person1.foo4().call(person2) // person1
 
 
--- 07_this其他补充/12_this面试题四.js
+++ 07_this其他补充/12_this面试题四.js
@@ -17,8 +17,8 @@ function Person (name) {
   }
 }
 
-var person1 = new Person('person1')
-var person2 = new Person('person2')
+var person1 = new Person('person1') // person1 = { name: person1, obj: { foo1, foo2 } }
+var person2 = new Person('person2') // person2 = { name: person2, obj: { foo1, foo2 } }
 
 person1.obj.foo1()() // window
 person1.obj.foo1.call(person2)() // window
