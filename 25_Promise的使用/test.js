8c199421d924f32fcf07ba78fd98fd1cd5989a13
ailee945 <ailee945@outlook.com>
2022-04-11 17:53:35 +0800

    promise-then

--- 25_Promise的使用/test.js
+++ 25_Promise的使用/test.js
@@ -1,11 +1,62 @@
-const promise = new Promise((resolve,reject)=>{
-  resolve('resolve---')
-  // reject('reject---')
-})
+const PROMISE_STATUS_PENDING = "pending";
+const PROMISE_STATUS_FULFILLED = "fulfilled";
+const PROMISE_STATUS_REJECTED = "rejected";
 
-promise.then((res)=>{
-  console.log(res);
-  throw new Error('reject then ---')
-}).catch((err)=>{
-  console.log(err); // reject ---; 优先调用第一个Promise的错误
-})
+class CustomPromis {
+  constructor(executor) {
+    this.status = PROMISE_STATUS_PENDING;
+    this.value = undefined;
+    this.reason = undefined;
+
+    const resolve = (value) => {
+      if (this.status === PROMISE_STATUS_PENDING) {
+        this.status = PROMISE_STATUS_FULFILLED;
+        queueMicrotask(() => {
+          this.value = value
+          this.onFulfilled(value);
+          console.log("resolve");
+        });
+      }
+    };
+    const reject = (reason) => {
+      if (this.status === PROMISE_STATUS_PENDING) {
+        this.status = PROMISE_STATUS_REJECTED;
+        queueMicrotask(() => {
+          this.reason = reason
+          this.onRejected(this.reason);
+          console.log("reject");
+        });
+      }
+    };
+
+    executor(resolve, reject);
+  }
+
+  then(onFulfilled, onRejected) {
+    this.onFulfilled = onFulfilled;
+    this.onRejected = onRejected;
+  }
+}
+
+// 功能测试
+const promise = new CustomPromis((resolve, reject) => {
+  resolve('resolve+++')
+  reject("reject+++");
+});
+
+promise.then(
+  (res) => {
+    console.log(res);
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+promise.then(
+  (res) => {
+    console.log(res);
+  },
+  (err) => {
+    console.log(err);
+  }
+);
