feb85cd23d7ee6aff8cd5a51ee31e3c3beb46dec
Ailee <ailee945@outlook.com>
2022-04-13 23:12:15 +0800

    模块化

--- 30_JS模块化解析/02_CommonJS/02_内部原理/main.js
+++ 30_JS模块化解析/02_CommonJS/02_内部原理/main.js
@@ -1,3 +1,4 @@
+const info = require("./why.js")
 const why = require("./why.js")
 
 console.log(why)
@@ -6,3 +7,6 @@ setTimeout(() => {
   // console.log(why.name)
   why.name = "james"
 }, 1000)
+
+
+// info module.exports why 指向同一个对象
