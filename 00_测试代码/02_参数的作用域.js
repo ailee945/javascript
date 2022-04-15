7a7a0ac144eb4bfdf87a33462b7ed84034ce96df
Ailee <ailee945@outlook.com>
2022-04-15 22:59:31 +0800

    dom

--- 00_测试代码/02_参数的作用域.js
+++ 00_测试代码/02_参数的作用域.js
@@ -1,4 +1,5 @@
 var x = 0
+// 参数按值传递和按照共享传递
 
 // 当函数的参数有默认值时, 会形成一个新的作用域, 这个作用域用于保存参数的值
 function foo(x, y = function() { x = 3; console.log(x) }) {
@@ -9,7 +10,7 @@ function foo(x, y = function() { x = 3; console.log(x) }) {
   console.log(x)
 }
 
-foo(1)
+foo()
 console.log(x)
 
 // undefined
---
+++ 35_DOM-Event/test.js
@@ -0,0 +1,11 @@
+const divEl = document.createElement('div')
+divEl.textContent = 'zhang'
+
+// const divEl2 = new HTMLDivElement()
+// divEl2.textContent = 'lee'
+
+// const imgEl = new Image()
+// imgEl.src = "../00_复习及面试/img/环境变量和记录.png";
+
+document.body.appendChild(divEl)
+// document.body.appendChild(divEl2)
