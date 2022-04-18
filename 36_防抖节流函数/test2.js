df4eacd26b53796446830f9ea6ca0a34bf899031
Ailee <ailee945@outlook.com>
2022-04-18 12:51:17 +0800

    throttle-trailing

--- 36_防抖节流函数/test2.js
+++ 36_防抖节流函数/test2.js
@@ -1,11 +1,24 @@
-function throttle(fn, interval) {
+function throttle(fn, interval, options = { leading: true, trailing: false }) {
+  const { leading, trailing } = options;
+  // 此时默认第一次会执行
   let lastTime = 0;
+  let timer = null
   function _throttle() {
+    // 当前事件触发的时间
     const nowTime = new Date().getTime();
+    // 第一次不触发
+    if (!lastTime && !leading) lastTime = nowTime;
     const remainTime = interval - (nowTime - lastTime);
     if (remainTime <= 0) {
       fn();
       lastTime = nowTime;
+      return // 防止添加额外定时器
+    }
+    // trailing为true 并且 前面没有定时器才执行
+    if(trailing && !timer){
+      setTimeout(() => {
+        fn()
+      }, remainTime);
     }
   }
   return _throttle;
@@ -17,3 +30,5 @@ const inputThrottlr = function () {
   console.log("防抖操作触发");
 };
 inputEle.oninput = throttle(inputThrottlr, 2000);
+
+// 34min25s
